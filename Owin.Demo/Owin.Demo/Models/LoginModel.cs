3d596f1d0c738e134d8528db1911e476087e0d0a
TheKingInDaNorth <60161055+TheKingInDaNorth@users.noreply.github.com>
2020-11-20 15:52:00 -0800

    11,20,20

---
+++ Owin.Demo/Owin.Demo/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Owin.Demo.Models
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public List<AuthenticationDescription> AuthProviders { get; internal set; }
+    }
+}
